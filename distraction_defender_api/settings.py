0f169e546b057770d09464784796e0d847c21843
DLRO92 <108065549+DLRO92@users.noreply.github.com>
2023-11-16 12:37:44 +0000

    allowed host

--- distraction_defender_api/settings.py
+++ distraction_defender_api/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = 'django-insecure-60c-d=8nn0q$ommtekhhjp2*7*kdjh3+(u8o^_5v8us509nifa
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'distraction-defender-server.onrender.com']
 
 
 # Application definition
