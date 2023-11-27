57bbd2746f06b3d6ea47650bfc5b9cba061faa63
DLRO92 <108065549+DLRO92@users.noreply.github.com>
2023-11-27 13:11:47 +0000

    login

--- apps/users/views.py
+++ apps/users/views.py
@@ -30,19 +30,23 @@ class Login(TokenObtainPairView):
     def post(self, request, *args, **kwargs):
         username = request.data.get('username', '')
         password = request.data.get('password', '')
-        user = authenticate(username = username, password=password)
+        user = authenticate(username=username, password=password)
+        keys_to_include = ['email', 'username', 'id']
         
         if user:
             login_serializer = self.serializer_class(data=request.data)
             if login_serializer.is_valid():
                 user_serializer = UserSerializer(user)
+                filtered_user_data = {
+                    key: user_serializer.data[key] for key in keys_to_include if key in user_serializer.data
+                }
                 return Response({
                     'token': login_serializer.validated_data.get('access'),
                     'refresh_token': login_serializer.validated_data.get('refresh'),
-                    'user': user_serializer.data,
+                    'user': filtered_user_data,
                     'message': 'User logged successfully',
                 }, status=status.HTTP_200_OK)
-        return Response({'error':'username or password is incorrect'}, status=status.HTTP_400_BAD_REQUEST)   
+        return Response({'error':'username or password is incorrect'}, status=status.HTTP_400_BAD_REQUEST)
         
 class Logout(GenericAPIView):
     
