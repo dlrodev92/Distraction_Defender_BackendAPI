b491ec8894c7f2d9fbbdd171aa3b10a114ee68ca
dlrodev92 <dlrdev92@gmail.com>
2024-04-04 22:00:49 +0100

    new images processors

--- distraction_defender_api/image_processor/image_processor.py
+++ distraction_defender_api/image_processor/image_processor.py
@@ -1,20 +1,41 @@
+from django.conf import settings
+import boto3
 from PIL import Image
 from io import BytesIO
-from django.core.files.uploadedfile import InMemoryUploadedFile
+
 
 def process_image(image_data):
         try:
-                # Process the image and return the path to the processed image
-                print("Processing image...")
-                print('this is the image file', image_data)
+                # Process the image 
                 image = Image.open(image_data)
                 output = BytesIO()
-                image = image.resize((300, 300))
+                image = image.resize((400, 400))
                 image.save(output, format='WEBP', quality=100)
                 output.seek(0)
-                processed_image = InMemoryUploadedFile(output, 'ImageField', "%s.webp" % image_data.name.split('.')[0], 'image/webp', output.tell(), None)
-                print('imagen processed image', processed_image)
-                return processed_image
+                
+                #Prepare the image ro be uploaded to aws S3
+                file_name = f"{image_data.name.split('.')[0]}.webp"
+                content_type = 'image/webp'
+                
+                 # Get an S3 client
+                s3 = boto3.client('s3', 
+                aws_access_key_id=settings.AWS_ACCESS_KEY_ID, 
+                aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+                region_name=settings.AWS_S3_REGION_NAME)
+
+                # Upload the image to S3
+                s3.upload_fileobj(
+                output,
+                settings.AWS_STORAGE_BUCKET_NAME,  
+                file_name,  
+                ExtraArgs={'ContentType': content_type}
+                )
+
+                # The URL to access the file on S3
+                file_url = f"https://{settings.AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/{file_name}"
+
+                print('Image processed and uploaded to S3:', file_url)
+                return file_url
         except Exception as e:
-                print("Error processing image: ", e)
+                print("Error processing and uploading image: ", e)
                 return None
--- distraction_defender_api/settings.py
+++ distraction_defender_api/settings.py
@@ -156,3 +156,14 @@ SIMPLE_JWT = {
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True
 }
+
+# aws3 credentials
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY =  os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = 'distractiondefender'
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+AWS_S3_REGION_NAME = 'eu-west-2'
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
